b8e07d8a3433aa25062836656d708f98bfa4333b
Helanen <henri.helanen@nokia.com>
2018-07-13 13:55:49 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './components/App.js';
+import reducer from "./reducers/reducer.js";
+
+var store = createStore(reducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+document.getElementById('root')
+);
+
